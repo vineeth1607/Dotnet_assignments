75ca09255a010f08d40c09d5c895befe06f4f298
vineeth1607 <vineethayaan@gmail.com>
2022-05-05 10:13:20 +0530

    code

---
+++ C#/codebase_test/CodeBasedTest1/CodeBasedTest1/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedTest1
+{
+    internal class Program
+    {
+        class Passenger
+        {
+            string name;
+            int age;
+            DateTime DateofTravel;
+            int no_of_tickets;
+            public Passenger(string name, int age, DateTime DateofTravel)
+            {
+                this.name = name;
+                this.age = age;
+                this.DateofTravel = DateofTravel;
+            }
+            public void TicketBooking(int no_of_tickets)
+            {
+
+                try
+                {
+                    Console.WriteLine($"name is {name} has booked  with DateofTravel {DateofTravel}");
+                    if (no_of_tickets > 5)
+                    {
+                        Console.WriteLine("cannot book more than 5 tickets");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ticket Booked Successfully");
+                    }
+
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("enter no_of_tickets in integers only");
+                }
+
+            }
+        }
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                try
+                {
+                    Passenger passenger = new Passenger("Vineeth", 23, Convert.ToDateTime("5/5/2022"));
+                    Console.WriteLine("enter the number of tickets you want to book: ");
+                    passenger.TicketBooking(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("not a valid imput");
+                }
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+}

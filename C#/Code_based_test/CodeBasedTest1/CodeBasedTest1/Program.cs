fcae054482940ae241c29542f58c71218295f469
vineeth1607 <vineethayaan@gmail.com>
2022-05-05 10:03:06 +0530

    codebase

--- C#/Code_based_test/CodeBasedTest1/CodeBasedTest1/Program.cs
+++ C#/Code_based_test/CodeBasedTest1/CodeBasedTest1/Program.cs
@@ -11,44 +11,57 @@ namespace CodeBasedTest1
     {
         class Passenger
         {
-            public String name { get; set; }
-            public int age { get; set; }
-            public String dateOfTravell { get; set; }
-            public  Passenger (String name , int age ,String dateOfTravell, int noOfTickets )
+            string name;
+            int age;
+            DateTime DateofTravel;
+            int no_of_tickets;
+            public Passenger(string name, int age, DateTime DateofTravel)
             {
                 this.name = name;
                 this.age = age;
-                this.dateOfTravell = dateOfTravell;
-                
-                
-
+                this.DateofTravel = DateofTravel;
             }
-            public static void TicketBooking (int noOfTickets)
-                {
-                noOfTickets = noOfTickets;
-                Console.WriteLine("Tickets booked successfully");
-            }
-        }
+            public void TicketBooking(int no_of_tickets)
+            {
 
-        public class Ticket
-            {
-            public int noOfTickets
-            {
-                get => noOfTickets;
-                set
+                try
                 {
-                    if (value > 5)
-                        throw new Exception("Can not book more than 5 tickets");
-                    noOfTickets = value;
+                    Console.WriteLine($"name is {name} has booked  with DateofTravel {DateofTravel}");
+                    if (no_of_tickets > 5)
+                    {
+                        Console.WriteLine("cannot book more than 5 tickets");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ticket Booked Successfully");
+                    }
+
                 }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("enter no_of_tickets in integers only");
+                }
+
             }
         }
-        static void Main(string[] args)
+        class Test
         {
-            Passenger pass = new Passenger ("Vineeth", 23, "05/05/2022",2);
-            Console.WriteLine("Enter the No.of tickets you want to book");
-            pass.TicketBooking(int.Parse(Console.ReadLine()));
-            Console.ReadLine();
+            static void Main(string[] args)
+            {
+                try
+                {
+                    Passenger passenger = new Passenger("Vineeth", 23, Convert.ToDateTime("5/5/2022"));
+                    Console.WriteLine("enter the number of tickets you want to book: ");
+                    passenger.TicketBooking(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("not a valid imput");
+                }
+                Console.ReadLine();
+
+            }
         }
+
     }
 }

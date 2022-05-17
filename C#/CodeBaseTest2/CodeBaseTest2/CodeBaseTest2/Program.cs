0ac92f9838c785213dcbc01b351ec8e2886932d0
vineeth1607 <vineethayaan@gmail.com>
2022-05-17 18:27:55 +0530

    codebasetest

---
+++ C#/CodeBaseTest2/CodeBaseTest2/CodeBaseTest2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBaseTest2
+{
+    class Program
+    {
+
+        public delegate void calculator(int x, int y);
+
+         class Programm
+        {
+            public static void addition(int x, int y)
+            {
+                int res = x + y;
+                Console.WriteLine($"result of addition : {res}");
+            }
+            public static void substraction(int x, int y)
+            {
+                int res = x - y;
+                Console.WriteLine($"result of substraction : {res}");
+            }
+            public static void multiplication(int x, int y)
+            {
+                int res = x * y;
+                Console.WriteLine($" result of multiplication : {res}");
+            }
+            public static void division(int x, int y)
+            {
+                int res = x / y;
+                Console.WriteLine($" result of division : {res}");
+            }
+            static void Main(string[] args)
+            {
+                calculator obj = new calculator(Programm.addition);
+                obj.Invoke(30, 40);
+                obj = substraction;
+                obj(30, 10);
+                obj = multiplication;
+                obj(20, 30);
+                obj = division;
+                obj(40, 20);
+                Console.ReadLine();
+            }
+        }
+    }
+}

746f87b85f457e1fbf6e49228ecd9595ab1a2938
vineeth1607 <vineethayaan@gmail.com>
2022-06-14 00:22:34 +0530

    MVC

---
+++ MVC_Customers/MVC_Customers/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Customers.Models;
+
+namespace MVC_Customers.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Customer_in_Germany()
+        {
+            List<Customer> customers = (from cust in db.Customers
+                                        where cust.Country == "Germany"
+                                        select cust).ToList();
+            return View(customers);
+        }
+        public ActionResult GetCustomers_ById()
+        {
+            List<Customer> customers = (from cust in db.Customers
+                                        join orders in db.Orders on cust.CustomerID equals orders.CustomerID
+                                        where orders.OrderID == 10248
+                                        select cust).ToList();
+            return View(customers);
+        }
+    }
+}
